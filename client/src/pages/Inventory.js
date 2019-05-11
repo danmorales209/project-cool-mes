a2fc20264ba8faaaa632069dac06d185b68ba3d1
Daniel Morales <danmorales209@gmail.com>
2019-05-11 12:27:15 -0700

    clean up the pages and React frontend

---
+++ client/src/pages/Inventory.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Inventory() {
+    return (
+        <>
+            <h1>This is the most boring Inventory page ever</h1>
+        </>
+    );
+};
+
+export default Inventory;
