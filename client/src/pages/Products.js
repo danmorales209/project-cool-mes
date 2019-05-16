8e2c7e86a850dcc02a2a712bfc14f9f54288403e
thedanielenrico <thedanielenrico@gmail.com>
2019-05-16 11:10:37 -0700

    some changes

--- client/src/pages/Products.js
+++ client/src/pages/Products.js
@@ -1,15 +1,9 @@
 import React, { Component } from "react";
 import {
   Card, CardImg, CardText, CardBody,
-<<<<<<< HEAD
-  CardTitle, CardSubtitle, Button
-} from 'reactstrap';
-import { Jumbotron } from 'reactstrap';
-=======
   CardTitle, CardSubtitle, Button, Jumbotron
 } from 'reactstrap';
 import { Col, Row, Container } from "../components/Grid";
->>>>>>> e16bd7615a00f0aaa1642e650eb06da377b26fb9
 // import API from "../utils/API";
 // import { Link } from "react-router-dom";
 import AddProductForm from "../components/AddProductForm"
@@ -28,15 +22,6 @@ class Products extends Component {
 
   render() {
     return (
-<<<<<<< HEAD
-      <Container fluid>
-        <Row>
-          <Col size="md-6">
-            <Jumbotron>
-              <h1>Add New Product</h1>
-              <Card>
-                <AddProductForm />
-=======
       <div className="container">
         <Container fluid>
           <Row>
@@ -52,7 +37,6 @@ class Products extends Component {
                   <CardText>New Description</CardText>
                   <Button>Create Product</Button>
                 </CardBody>
->>>>>>> e16bd7615a00f0aaa1642e650eb06da377b26fb9
               </Card>
             </Col>
             <Col size="md-3">
