dd4e7f16862c213aac40be73c6d87ef7ccb28b32
Lindca <45475418+Lindca@users.noreply.github.com>
2019-05-15 19:40:45 -0700

    fixed a lot of columns and rows and added an extensive form to the order page

--- client/src/pages/Orders.js
+++ client/src/pages/Orders.js
@@ -7,8 +7,8 @@ import { Col, Row, Container } from "../components/Grid";
 // import API from "../utils/API";
 // import { Link } from "react-router-dom";
 import OrderCard from "../components/OrderCards";
-// import { List, ListItem } from "../components/List";
-// import { Input, TextArea, FormBtn } from "../components/Form";
+import AddOrderForm from "../components/AddOrderForm";
+
 
 class Orders extends Component {
   state = {
@@ -23,53 +23,65 @@ class Orders extends Component {
 
   render() {
     return (
-      <Container fluid>
-        <Row>
-          <Col size="md-12">
-            <h1>New Orders</h1>
-          </Col>
-        </Row>
-        <Row>
-          <Col size="md-3">
-            <Card>
-              <CardBody>
-                <CardTitle>New Order</CardTitle>
-                <CardText>New Order</CardText>
-                <Button>Start New Order</Button>
-              </CardBody>
-            </Card>
-          </Col>
-          <Col size="md-3">
-            <OrderCard>
-              A card for each Active order in the database.
+      <div className="container">
+        <Container fluid>
+          <Row>
+            <Col size="md-12">
+              <h1>New Orders</h1>
+            </Col>
+          </Row>
+          <Row>
+            <Col size="md-3">
+              <Card>
+                <CardBody>
+                  <CardTitle>New Order</CardTitle>
+                  <CardText>New Order</CardText>
+                  <Button>Start New Order</Button>
+                </CardBody>
+              </Card>
+            </Col>
+            <Col size="md-3">
+              <OrderCard>
+                A card for each Active order in the database.
             </OrderCard>
-          </Col>
-        </Row>
-        <Row>
-          <Col size="md-12">
-            <h1>Queued Orders</h1>
-          </Col>
-        </Row>
-        <Row>
-          <Col size="md-3">
-            <OrderCard>
-              A card for each Queued order in the database.
+            </Col>
+          </Row>
+          <Row>
+            <Col size="md-12">
+              <h1>Queued Orders</h1>
+            </Col>
+          </Row>
+          <Row>
+            <Col size="md-3">
+              <OrderCard>
+                A card for each Queued order in the database.
             </OrderCard>
-          </Col>
-        </Row>
-        <Row>
-          <Col size="md-12">
-            <h1>Completed Orders</h1>
-          </Col>
-        </Row>
-        <Row>
-          <Col size="md-3">
-            <OrderCard>
-              A card for each Completed order in the database.
+            </Col>
+          </Row>
+          <Row>
+            <Col size="md-12">
+              <h1>Completed Orders</h1>
+            </Col>
+          </Row>
+          <Row>
+            <Col size="md-3">
+              <OrderCard>
+                A card for each Completed order in the database.
             </OrderCard>
-          </Col>
-        </Row>
-      </Container>
+            </Col>
+          </Row>
+          <Row>
+            <Col size="md-12">
+              <h1>Order Form</h1>
+            </Col>
+          </Row>
+          <Row>
+            <Col size="md-6">
+              <AddOrderForm />
+            </Col>
+          </Row>
+        </Container>
+      </div>
     );
   }
 }
--- client/src/pages/Products.js
+++ client/src/pages/Products.js
@@ -22,37 +22,39 @@ class Products extends Component {
 
   render() {
     return (
-      <Container fluid>
-        <Row>
-          <Col size="md-12">
-            <h1>Current Products</h1>
-          </Col>
-        </Row>
-        <Row>
-          <Col size="md-3">
-            <Card>
-              <CardBody>
-                <CardTitle>New Product Name</CardTitle>
-                <CardText>New Description</CardText>
-                <Button>Create Product</Button>
-              </CardBody>
-            </Card>
-          </Col>
-          <Col size="md-3">
-            <ProductCard />
-          </Col>
-        </Row>
-        <Row>
-          <Col size="md-12">
-            <h1>Product Form</h1>
-          </Col>
-        </Row>
-        <Row>
-          <Col size="md-6">
-            <AddProductForm />
-          </Col>
-        </Row>
-      </Container>
+      <div className="container">
+        <Container fluid>
+          <Row>
+            <Col size="md-12">
+              <h1>Current Products</h1>
+            </Col>
+          </Row>
+          <Row>
+            <Col size="md-3">
+              <Card>
+                <CardBody>
+                  <CardTitle>New Product Name</CardTitle>
+                  <CardText>New Description</CardText>
+                  <Button>Create Product</Button>
+                </CardBody>
+              </Card>
+            </Col>
+            <Col size="md-3">
+              <ProductCard />
+            </Col>
+          </Row>
+          <Row>
+            <Col size="md-12">
+              <h1>Product Form</h1>
+            </Col>
+          </Row>
+          <Row>
+            <Col size="md-6">
+              <AddProductForm />
+            </Col>
+          </Row>
+        </Container>
+      </div>
     );
   }
 }
