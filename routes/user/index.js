8309c38ff5a96264261bb4534419e681b9430e60
Daniel Morales <danmorales209@gmail.com>
2019-05-18 10:00:54 -0700

    add fetch call to App component didMount

--- routes/user/index.js
+++ routes/user/index.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const userController = require("../../controllers/userController");
 
-
+// LOGIN & SIGNUP ROUTES //
 router.route('/login').post(userController.login);
 router.route('/signup').post(userController.signup);
 router.route('/validated').post(userController.validateToken);
