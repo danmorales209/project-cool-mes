5f28bf7fae80e67b90b66c4c03e99834f9886d74
thedanielenrico <thedanielenrico@gmail.com>
2019-05-18 14:48:55 -0700

    Do some shit to the inventory page

--- client/src/pages/Inventory.js
+++ client/src/pages/Inventory.js
@@ -6,67 +6,97 @@ import axios from "axios";
 // import API from "../utils/API";
 // import { Link } from "react-router-dom";
 import { Col, Row, Container } from "../components/Grid";
-// import InventoryInput from "../components/InventoryInput";
-// import { List, ListItem } from "../components/List";
-// import { Input, TextArea, FormBtn } from "../components/Form";
+
 
 class Inventory extends Component {
   state = {
-    rawMatInv: [],
-    EqupInv: []
+    materialName: '',
+    materialQuantity: '',
+    materialUnit: '',
+    equipmentName: '',
+    equipmentType: '',
   };
 
-  handlePostEquipment() {
-    axios.post("/api/inventory/POST", this.state.EqupInv).then(res => {
+  handlePostMaterial = () => {
+    axios.post("/api/inventory/POST", {
+      name: this.state.materialName,
+      quantity: this.state.materialQuantity,
+      unit: this.state.materialUnit
+    }).then(res => {
       console.log(res.data)
     })
   }
-  handlePostMaterial() {
-    axios.post("/api/inventory/POST", this.state.rawMatInv).then(res => {
+  handlePostEquipment() {
+    axios.post("/api/equipment/POST", this.state.EqupInv).then(res => {
       console.log(res.data)
     })
   }
   componentDidMount() {
     // this.loadBooks();
   }
-  handleMaterialChange = (e) => {
-    this.setState({ rawMatInv: e.target.value })
+  handleInputChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value })
   }
-  handleEquipmentChange = (e) => {
-    this.setState({ EqupInv: e.target.value })
-  }
-  // handleClick = () => {
-  //   console.log(this.state)
-  // }
+
   render() {
     return (
       <div className="container">
         <Container fluid>
           <Row>
-            <Col size="md-12">
+            <Col size="md-6">
               <Jumbotron>
                 <h1>Raw Materials Inventory</h1>
                 <InputGroup>
-                  <h2>Item Name </h2>
-                  <Input placeholder="value" onChange={this.handleMaterialChange} />
-                  <InputGroupAddon addonType="append">
-                    <Button color="success" onClick={this.handlePostMaterial} >Update</Button>
-                  </InputGroupAddon>
+                  <h2>Material Name</h2>
+                  <Input
+                    placeholder="name"
+                    name="materialName"
+                    onChange={this.handleInputChange}
+                  />
                 </InputGroup>
+                <InputGroup>
+                  <h2>Amount of Material</h2>
+                  <Input
+                    placeholder="quantity"
+                    name="materialQuantity"
+                    type="number"
+                    onChange={this.handleInputChange}
+                  />
+                </InputGroup>
+                <InputGroup>
+                  <h2>Unit of Measure</h2>
+                  <Input
+                    placeholder="units"
+                    name="materialUnit"
+                    type="text"
+                    onChange={this.handleInputChange}
+                  />
+                </InputGroup>
+                <Button color="success" onClick={this.handlePostMaterial} >Update</Button>
               </Jumbotron>
             </Col>
           </Row>
           <Row>
-            <Col size="md-12">
+            <Col size="md-6">
               <Jumbotron>
                 <h1>Equipment Inventory</h1>
                 <InputGroup>
-                  <h2>Item Name </h2>
-                  <Input placeholder="value" onChange={this.handleEquipmentChange} />
-                  <InputGroupAddon addonType="append">
-                    <Button color="success" onClick={this.handlePostEquipment} >Update</Button>
-                  </InputGroupAddon>
+                  <h2>Equipment Name </h2>
+                  <Input
+                    placeholder="name"
+                    name="equipmentName"
+                    onChange={this.handleInputChange}
+                  />
                 </InputGroup>
+                <InputGroup>
+                  <h2>Equipment Type </h2>
+                  <Input
+                    placeholder="type"
+                    name="equipmentType"
+                    onChange={this.handleInputChange}
+                  />
+                </InputGroup>
+                <Button color="success" onClick={this.handlePostEquipment} >Update</Button>
               </Jumbotron>
             </Col>
           </Row>
