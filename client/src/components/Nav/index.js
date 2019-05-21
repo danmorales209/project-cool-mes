40af04021c9e0e67ee0af201ff100991bbd8764f
Lindca <45475418+Lindca@users.noreply.github.com>
2019-05-20 18:37:15 -0700

    home page

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,21 +1,48 @@
-import React from "react";
+import React from 'react';
+import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
 
-function Nav() {
-  return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-      <div className="collapse navbar-collapse" id="navbar-collapse-01">
-        <ul className="nav navbar-nav">
-          <li><a className="navbar-brand" href="/">Home</a></li>
-          <li><a className="navbar-brand" href="/products">Products</a></li>
-          <li><a className="navbar-brand" href="/orders">Orders</a></li>
-          <li><a className="navbar-brand" href="/inventory">Inventory</a></li>
-        </ul>
-        <ul className="nav navbar-nav navbar-right">
-          <li><a className="navbar-brand" href="/login">Login</a></li>
-        </ul>
+export default class Example extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggleNavbar = this.toggleNavbar.bind(this);
+    this.state = {
+      collapsed: true
+    };
+  }
+
+  toggleNavbar() {
+    this.setState({
+      collapsed: !this.state.collapsed
+    });
+  }
+  render() {
+    return (
+      <div>
+        <Navbar color="faded" light>
+          <NavbarBrand href="/" className="mr-auto">MES</NavbarBrand>
+          <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
+          <Collapse isOpen={!this.state.collapsed} navbar>
+            <Nav navbar>
+              <NavItem>
+                <NavLink href="/">Home</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/products">Products</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/orders">Orders</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/inventory">Inventory</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/login">Log In</NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
       </div>
-    </nav>
-  );
+    );
+  }
 }
-
-export default Nav;
