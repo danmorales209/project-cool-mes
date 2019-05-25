f12d1c0a9430ca3d5b981feaa11ad0931411d3f1
Lindca <45475418+Lindca@users.noreply.github.com>
2019-05-25 11:03:16 -0700

    working on order page

--- client/src/components/AddOrderForm/index.js
+++ client/src/components/AddOrderForm/index.js
@@ -7,45 +7,37 @@ const AddOrderForm = (props) => {
         <Form>
             <FormGroup>
                 <Label for="productName">Product Name</Label>
-                <Input type="" name="productName" id="productName" onChange={props.handleInputChange} placeholder="Product Name" />
+                <Input type="" name="productName" id="productName" onChange={(e) => this.handleInputChange(e.target.value)} placeholder="Product Name" />
             </FormGroup>
             <FormGroup>
                 <Label for="unitsNeeded">Total Units Needed</Label>
-                <Input type="" name="Units Needed" id="unitsNeeded" onChange={props.handleInputChange} placeholder="Total Units Needed" />
-            </FormGroup>
-            <FormGroup>
-                <Label for="unitsAvailable">Current Units Available</Label>
-                <Input type="" name="unitsAvailable" id="unitsAvailable" onChange={props.handleInputChange} placeholder="Current Units Available" />
-            </FormGroup>
-            <FormGroup>
-                <Label for="addUnitsNeeded">Additional Units Needed</Label>
-                <Input type="" name="addUnitsNeeded" id="addUnitsNeeded" onChange={props.handleInputChange} placeholder="Additional Units Needed" />
+                <Input type="" name="Units Needed" id="unitsNeeded" onChange={(e) => this.handleInputChange(e.target.value)} placeholder="Total Units Needed" />
             </FormGroup>
             <FormGroup>
                 <Label for="dueDate">Due Date</Label>
-                <Input type="" name="dueDate" id="dueDate" onChange={props.handleInputChange} placeholder="Due Date" />
+                <Input type="" name="dueDate" id="dueDate" onChange={(e) => this.handleInputChange(e.target.value)} placeholder="Due Date" />
             </FormGroup>
             <FormGroup>
                 <Label for="customerName">Customer Name</Label>
-                <Input type="text" name="customerName" id="customerName" onChange={props.handleInputChange} placeholder="Jane Doe" />
+                <Input type="text" name="customerName" id="customerName" onChange={(e) => this.handleInputChange(e.target.value)} placeholder="Jane Doe" />
             </FormGroup>
             <FormGroup>
                 <Label for="exampleAddress">Address</Label>
-                <Input type="text" name="address" id="address" onChange={props.handleInputChange} placeholder="1234 Main St" />
+                <Input type="text" name="address" id="address" onChange={(e) => this.handleInputChange(e.target.value)} placeholder="1234 Main St" />
             </FormGroup>
             <FormGroup>
                 <Label for="exampleCity">City</Label>
-                <Input type="text" name="city" onChange={props.handleInputChange} id="exampleCity" />
+                <Input type="text" name="city" onChange={(e) => this.handleInputChange(e.target.value)} id="exampleCity" />
             </FormGroup>
             <FormGroup>
                 <Label for="exampleState">State</Label>
-                <Input type="text" name="state" onChange={props.handleInputChange} id="exampleState" />
+                <Input type="text" name="state" onChange={(e) => this.handleInputChange(e.target.value)} id="exampleState" />
             </FormGroup>
             <FormGroup>
                 <Label for="exampleZip">Zip</Label>
-                <Input type="text" name="zip" onChange={props.handleInputChange} id="exampleZip" />
+                <Input type="text" name="zip" onChange={(e) => this.handleInputChange(e.target.value)} id="exampleZip" />
             </FormGroup>
-            <Button>Submit</Button>
+            <Button color="success" onClick={props.handlePostOrder}>Submit</Button>
         </Form>
     );
 }
