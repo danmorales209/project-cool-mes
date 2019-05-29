8c7e61b1475ba765cca1a76b800ebfbb070c5d86
Lindca <45475418+Lindca@users.noreply.github.com>
2019-05-28 20:39:39 -0700

    order page almost done, mapping through orders

--- models/order.js
+++ models/order.js
@@ -6,24 +6,6 @@ const orderSchema = new Schema(
     product: {
       type: Schema.ObjectId,
       ref: "Recipe"
-<<<<<<< HEAD
-    }
-  ],
-  status: {
-    type: String,
-    default: "New" //In Progress, Completed
-  },
-  customer: {
-    name: String,
-    address: String,
-    city: String,
-    state: String,
-    zip: Number
-  },
-  dueDate: Date,
-  qtyNeeded: Number
-});
-=======
     },
     inProgress: {
       type: String,
@@ -45,7 +27,6 @@ const orderSchema = new Schema(
     qtyNeeded: Number
   }
 );
->>>>>>> 57de3e7018950dadc7133b08a39f06df16a39be5
 
 const Order = mongoose.model("Order", orderSchema);
 
