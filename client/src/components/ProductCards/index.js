d8b3144508427bfcabe7129e71e1e3eca18aca0c
Lindca <45475418+Lindca@users.noreply.github.com>
2019-05-30 20:30:52 -0700

    added alot of stuff for the product page like the form is moved and the cards work etc.

--- client/src/components/ProductCards/index.js
+++ client/src/components/ProductCards/index.js
@@ -3,17 +3,15 @@ import {
     Card, CardImg, CardText, CardBody,
     CardTitle, CardSubtitle, Button, Jumbotron
 } from 'reactstrap';
-import { Col, Row, Container } from ".././Grid";
 
 const ProductCard = (props) => {
     return (
         <div>
             <Card>
                 <CardBody>
-                    <CardTitle>Product Name: </CardTitle>
-                    <CardText>Product Description: </CardText>
-                    <CardText>Steps:</CardText>
-                    <Button>Open</Button>
+                    <CardTitle>Product Name: {props.obj.name}</CardTitle>
+                    <CardText>Product Description: {props.obj.description}</CardText>
+                    <Button onClick={this.openProduct}>Open</Button>
                 </CardBody>
             </Card>
         </div>
--- client/src/pages/Products.js
+++ client/src/pages/Products.js
@@ -1,29 +1,54 @@
 import React, { Component } from "react";
 import {
-  Card, CardImg, CardText, CardBody,
-  CardTitle, CardSubtitle, Button, Form, FormGroup, Label, Input, FormText
+  Button, Form, FormGroup, Label, Input, Dropdown, DropdownToggle, DropdownMenu, DropdownItem, Table
 } from 'reactstrap';
 import { Col, Row, Container } from "../components/Grid";
-// import API from "../utils/API";
-// import { Link } from "react-router-dom";
 import axios from "axios";
-// import AddProductForm from "../components/AddProductForm"
 import ProductCard from "../components/ProductCards";
 
 class Products extends Component {
   state = {
-    productsArray: [],
-    newProduct: "",
-    newProductDesc: "",
+    allProducts: [],
     steps: [],
     productName: '',
-    productDescription: '',
+    description: '',
+    equipment: "",
+    inventory: "",
+    allEquipment: [],
+    allInventory: [],
+    dropdownOpen:"",
   };
 
   componentDidMount() {
-    // this.loadBooks();
+    this.loadEquipment();
+    this.loadInventory();
+    this.loadProducts();
   }
-
+  loadEquipment = () => {
+    axios.get("/api/equipment/GET").then(res => {
+      this.setState({ allEquipment: res.data });
+    });
+  }
+  loadInventory = () => {
+    axios.get("/api/inventory/GET").then(res => {
+      this.setState({ allInventory: res.data });
+    });
+  }
+  loadProducts = () => {
+    axios.get("/api/recipe/GET").then((res) => {
+      this.setState({ allProducts: res.data }, ()=>{(console.log(this.state.allProducts,"line 39"))});
+    })
+  }
+  toggleInventory = () => {
+    this.setState(prevState => ({
+      dropdownOpen: !prevState.dropdownOpen
+    }));
+  };
+  toggleEquipment = () => {
+    this.setState(prevState => ({
+      dropdownOpen: !prevState.dropdownOpen
+    }));
+  };
   handleAddStep = () => {
     let steps = this.state.steps;
     steps.push("");
@@ -34,13 +59,8 @@ class Products extends Component {
       })
     )
   }
-  handleName = (e) => {
-    this.setState({ productName: e.target.value })
-
-  }
-  handleDescription = (e) => {
-    this.setState({ productDescription: e.target.value })
-
+  handleInputChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value })
   }
   handleChange = (e) => {
     let steps = this.state.steps;
@@ -49,11 +69,9 @@ class Products extends Component {
     this.setState({ steps: steps })
   }
   handleDelete = (e) => {
-
     let steps = this.state.steps;
     let id = e.target.id;
     steps = steps.filter((el, index) => index !== +id)
-
     this.setState({ steps: steps })
   }
   handleSubmit = () => {
@@ -69,19 +87,7 @@ class Products extends Component {
             </Col>
           </Row>
           <Row>
-            <Col size="md-3">
-              <Card>
-                <CardBody>
-                  <CardTitle>New Product Name:</CardTitle>
-                  <CardText>New Product Description:</CardText>
-                  <CardText>Steps:</CardText>
-                  <Button>Create Product</Button>
-                </CardBody>
-              </Card>
-            </Col>
-            <Col size="md-3">
-              <ProductCard />
-            </Col>
+            {this.state.allProducts.map((data, i) => <Col size="md-3"><ProductCard obj={data} key={i}></ProductCard></Col>)}
           </Row>
           <Row>
             <Col size="md-12">
@@ -93,27 +99,74 @@ class Products extends Component {
               <Form>
                 <FormGroup>
                   <Label for="productName">Product Name</Label>
-                  <Input onChange={this.handleName} name="productName" id="productName" placeholder="Add product name" />
+                  <Input
+                    onChange={this.handleInputChange}
+                    name="productName"
+                    id="productName"
+                    placeholder="Add Product Name"
+                  />
                 </FormGroup>
                 <FormGroup>
-                  <Label for="productDescription">Add Description</Label>
-                  <Input onChange={this.handleDescription} name="productDescription" id="productDescription" placeholder="Add some info..." />
+                  <Label for="description">Add Description</Label>
+                  <Input
+                    onChange={this.handleInputChange}
+                    name="description"
+                    id="description"
+                    placeholder="Add A Description..."
+                  />
                 </FormGroup>
                 <FormGroup>
                   <Label for="stepsInput">Steps</Label>
-                  <br></br>
-                  {this.state.steps.map((el, index) =>
+                  <br />
+                  {this.state.steps.map((el, index) => (
                     <div>
                       <Input
                         value={this.state.steps[index]}
                         id={index}
                         key={index}
-                        name={"stepsInput" + index}
-                        placeholder="Add step info..."
-                        onChange={this.handleChange}>
-                      </Input>
-                      <Button id={index} onClick={this.handleDelete}>Remove Step</Button>
-                    </div>)}
+                        name={"directionsInput" + index}
+                        placeholder="Directions"
+                        onChange={this.handleChange}
+                      />
+                      <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggleInventory}>
+                        <DropdownToggle caret>Add Ingredients</DropdownToggle>
+                        <DropdownMenu>
+                          {this.state.allInventory.map((el, i) => (
+                            <>
+                              <DropdownItem
+                                name={el.inventory}
+                                key={i}
+                                id={el._id}
+                                onClick={this.select}
+                              >
+                                Type: {el.inventory}
+                              </DropdownItem>
+                              <DropdownItem divider />
+                            </>
+                          ))}
+                        </DropdownMenu>
+                      </Dropdown>
+                      <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggleEquipment}>
+                        <DropdownToggle caret>Add Equpiment</DropdownToggle>
+                        <DropdownMenu>
+                          {this.state.allEquipment.map((el, i) => (
+                            <>
+                              <DropdownItem
+                                name={el.equipment.name}
+                                key={i}
+                                id={el._id}
+                                onClick={this.select}
+                              >
+                                Type: {el.equipment.name}
+                              </DropdownItem>
+                              <DropdownItem divider />
+                            </>
+                          ))}
+                        </DropdownMenu>
+                      </Dropdown>
+                      <Button id={index} onClick={this.handleDelete}> Remove Step </Button>
+                    </div>
+                  ))}
                   <Button onClick={this.handleAddStep}>Add Step</Button>
                 </FormGroup>
                 <Button onClick={this.handleSubmit}>Submit</Button>
