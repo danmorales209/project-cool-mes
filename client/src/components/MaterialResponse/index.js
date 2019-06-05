349679d67a70e4536cf4a688a02856e834b366c3
MetalJester <acjswanson@gmail.com>
2019-06-04 20:22:04 -0700

    added qty to add buttons on inventory page

--- client/src/components/MaterialResponse/index.js
+++ client/src/components/MaterialResponse/index.js
@@ -1,34 +1,41 @@
 import React from "react";
-import { Table, Button } from 'reactstrap';
+import { Table, Button } from "reactstrap";
 
-const MaterialCard = (props) => {
-    return (
-        <Table>
-            <thead>
-                <tr>
-                    <th>#</th>
-                    <th>Name</th>
-                    <th>Quantity</th>
-                    <th>Units</th>
-                    <th>Add More</th>
-                </tr>
-            </thead>
-            <tbody>
-                {props.obj.map((el, i) =>
-                    <tr key={i}>
-                        <th scope="row">{i + 1}</th>
-                        <td>{el.name}</td>
-                        <td>{el.quantity}</td>
-                        <td>{el.units}</td>
-                        <td>
-                            <Button onClick={() => props.increaseBtn(el._id)}>Add 1</Button>
-                        </td>
-                    </tr>
-                )}
-
-            </tbody>
-        </Table>
-    );
+const MaterialCard = props => {
+  return (
+    <Table>
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Name</th>
+          <th>Quantity</th>
+          <th>Units</th>
+          <th>Add Qty</th>
+          <th>--</th>
+        </tr>
+      </thead>
+      <tbody>
+        {props.obj.map((el, i) => (
+          <tr key={i}>
+            <th scope="row">{i + 1}</th>
+            <td>{el.name}</td>
+            <td>{el.quantity}</td>
+            <td>{el.units}</td>
+            <td>
+              <input
+                className="add-equipment-field"
+                onChange={e => props.onChange(e)}
+                name={el._id}
+              />
+            </td>
+            <td>
+              <Button onClick={() => props.increaseBtn(el._id)}>Add</Button>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </Table>
+  );
 };
 
 export default MaterialCard;
