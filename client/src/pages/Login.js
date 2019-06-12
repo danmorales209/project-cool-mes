3e317e2b027c85aa1c4e2ae2f93e9b13fc51b669
Daniel Morales <danmorales209@gmail.com>
2019-06-11 19:42:42 -0700

    add signup stuff to login page

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,9 +1,8 @@
 import React, { Component } from "react";
 import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 import { Col, Row, Container } from "../components/Grid";
-import { BrowserRouter as Redirect } from "react-router-dom";
 import axios from 'axios';
-//import LogInCarousel from "../components/LogInCarousel";
+import LogInCarousel from "../components/LogInCarousel";
 
 export default class Login extends React.Component {
   constructor(props) {
@@ -13,8 +12,8 @@ export default class Login extends React.Component {
 
     this.state = {
       email: "",
-      password: ""
-
+      password: "",
+      verfiy: ""
     }
   };
 
@@ -25,17 +24,22 @@ export default class Login extends React.Component {
   handleEmailChange = (e) => {
     this.setState({ email: e.target.value })
   }
+
   handlePWChange = (e) => {
     this.setState({ password: e.target.value })
   }
 
-  handleSubmit = () => {
+  handleVerifyChange = (e) => {
+    this.setState({ verify: e.target.value });
+  }
+
+  handleLogin = () => {
     let data = {
       email: this.state.email,
       password: this.state.password
     };
-    this.login(data);
 
+    this.login(data);
 
   }
 
@@ -51,34 +55,47 @@ export default class Login extends React.Component {
         this.updateUser(response.data.email, response.data.token, () => {
           this.authorizeUser(true);
 
-          localStorage.setItem("user", JSON.stringify(response.data.email))
-          localStorage.setItem("token", JSON.stringify(response.data.token))
+          localStorage.setItem("user", JSON.stringify(response.data.email));
+          localStorage.setItem("token", JSON.stringify(response.data.token));
 
+          this.props.history.push(this.props.history.location.pathname);
+          document.location.replace(this.props.history.location.pathname);
 
-          this.props.history.push(this.props.history.location.pathname)
-          document.location.replace(this.props.history.location.pathname)
-        })
+        });
       }
     });
   };
 
-  signup = (route, data) => {
+  handleSignUp = () => {
+    let data = {
+      email: this.state.email,
+      password: this.state.password
+    }
+
+    this.signup(data);
+  }
+
+  signup = (data) => {
     console.log("trying to sign-up");
 
-    axios(route, data).then(response => {
-      this.setState(
-        { user: response.data.email, token: response.data.token },
-        () => {
-          console.log(this.state);
+    let route = "/user/signup";
 
-          console.log(this.validUser());
-        }
-      );
+    axios.post(route, data).then(response => {
+      console.log("Trying to send some data to login");
+
+      if (response.data) {
+        this.updateUser(response.data.email, response.data.token, () => {
+          this.authorizeUser(true);
+
+          localStorage.setItem("user", JSON.stringify(response.data.email));
+          localStorage.setItem("token", JSON.stringify(response.data.token));
+
+          this.props.history.push(this.props.history.location.pathname);
+          document.location.replace(this.props.history.location.pathname);
+
+        });
+      }
     });
-  };
-
-  handleSignUp = data => {
-    this.signup("/user/signup", data);
   };
 
   componentDidMount = () => {
@@ -115,33 +132,74 @@ export default class Login extends React.Component {
   render() {
     return (
       <div className="container">
+
         <Row>
           <Col size="md-12">
-            {/* <LogInCarousel></LogInCarousel> */}
+            <LogInCarousel />
           </Col>
         </Row>
+
         <Row>
           <Col size="md-12">
             <h1>Log In</h1>
           </Col>
         </Row>
+
         <Form>
           <Row form>
             <Col size="md-6">
               <FormGroup>
-                <Label for="exampleEmail">Email</Label>
-                <Input type="email" name="email" id="exampleEmail" placeholder="something@somewhere.com" onChange={(e) => this.handleEmailChange(e)} />
+                <Label for="loginEmail">Email</Label>
+                <Input type="email" name="loginEmail" id="loginEmail" placeholder="something@somewhere.com" onChange={(e) => this.handleEmailChange(e)} />
               </FormGroup>
             </Col>
             <Col size="md-6">
               <FormGroup>
-                <Label for="examplePassword">Password</Label>
-                <Input type="password" name="password" id="examplePassword" placeholder="Secret Code" onChange={(e) => this.handlePWChange(e)} />
+                <Label for="loginPassword">Password</Label>
+                <Input type="password" name="loginPassword" id="loginPassword" placeholder="Secret Code" onChange={(e) => this.handlePWChange(e)} />
               </FormGroup>
             </Col>
           </Row>
-          <Button onClick={() => this.handleSubmit()}>Sign in</Button>
+          <Button onClick={() => this.handleLogin()}>Sign in</Button>
         </Form>
+
+        {this.state.authorized === true
+          ? null
+          : (<>
+            <Row>
+              <Col size="md-12">
+                <h1>Sign up</h1>
+              </Col>
+            </Row>
+
+            <Form>
+              <Row form>
+                <Col size="md-12">
+                  <FormGroup>
+                    <Label for="signUpEmail">Email</Label>
+                    <Input type="email" name="email" id="signUpEmail" placeholder="Please enter your email" onChange={(e) => this.handleEmailChange(e)} />
+                  </FormGroup>
+                </Col>
+              </Row>
+              <Row form>
+                <Col size="md-6">
+                  <FormGroup>
+                    <Label for="signUpPassword">Password</Label>
+                    <Input type="password" name="password" id="signUpPassword" placeholder="Please enter your password " onChange={(e) => this.handlePWChange(e)} />
+                  </FormGroup>
+                </Col>
+                <Col size="md-6">
+                  <FormGroup>
+                    <Label id="verifyMessage" for="verifyPassword">Please Verify Your Password</Label>
+                    <Input type="password" name="password" id="verifyPassword" placeholder="Please verify your password " onChange={(e) => this.handleVerifyChange(e)} />
+                  </FormGroup>
+                </Col>
+              </Row>
+              <Button onClick={() => this.handleSignUp()}>Sign up</Button>
+            </Form>
+          </>
+          )}
+
       </div>
     );
   }
