52ff16cc952c6c7c40d625bb14f18c18e4545fa4
danmorales209 <danmorales209@gmail.com>
2019-06-12 21:12:32 -0700

    add recipeInventory to routes index

--- routes/api/index.js
+++ routes/api/index.js
@@ -3,6 +3,7 @@ const router = require("express").Router();
 const equipmentRoute = require("./equipment");
 const instructionsRoute = require("./instructions");
 const inventoryRoute = require("./inventory");
+const recipeInventoryRoute = require("./recipeInventory");
 const orderRoute = require("./order");
 const recipeRoute = require("./recipe");
 
@@ -12,6 +13,7 @@ router.use("/instructions", instructionsRoute);
 router.use("/inventory", inventoryRoute);
 router.use("/order", orderRoute);
 router.use("/recipe", recipeRoute);
+router.use("/recipeInventory", recipeInventoryRoute);
 
 // If no API routes are hit, send the React app
 router.use(function(req, res) {
